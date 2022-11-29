fad4ced75e8017dbd95e98dbf43f34e8e84988a2
hp <hp@HP>
2022-11-29 20:50:17 +0300

    Добавьте файлы проекта.

---
+++ Pr 12. n1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Pr_12._n1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 2, 3, 1, 4 };
+            Array.Sort(arr);
+            if (arr.Length > 0)
+            {
+                Console.WriteLine("Минимальное число: " + arr[0]);
+                Console.ReadKey();
+            }
+        }
+    }
+}
